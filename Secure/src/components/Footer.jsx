81d8401cc7d90ff3420765b86594bcd9012dc4e5
SHAIKHASRASWALEH <123099813+SHAIKHASRASWALEH@users.noreply.github.com>
2024-06-29 15:10:10 +0530

    Add files via upload

---
+++ Secure/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+                <span className='text-purple-500'> &lt;</span>
+
+                <span>SECURE</span><span className='text-purple-500'>IT/&gt;</span>
+
+
+            </div>
+           
+        </div>
+    )
+}
+
+export default Footer
